96d773cb33348492d04d46df480e3ec9eb9a8ac6
prasad <prasadbhupathi04@gmail.com>
2026-03-12 23:57:50 +0530

    Initial commit - User portal project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,58 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('user-management-portal');
+  page = signal('login');
+
+  username = '';
+  password = '';
+
+  loginUser = '';
+  loginPass = '';
+
+  users: any[] = [];
+
+  loggedUser = '';
+
+  signup() {
+    const user = {
+      username: this.username,
+      password: this.password,
+    };
+
+    this.users.push(user);
+
+    alert('Signup Successful');
+
+    this.page.set('login');
+
+    this.username = '';
+    this.password = '';
+  }
+
+  login() {
+    const user = this.users.find(
+      (u) => u.username === this.loginUser && u.password === this.loginPass,
+    );
+
+    if (user) {
+      this.loggedUser = user.username;
+
+      this.page.set('dashboard');
+    } else {
+      alert('Invalid Credentials');
+    }
+  }
+
+  logout() {
+    this.page.set('login');
+  }
 }
